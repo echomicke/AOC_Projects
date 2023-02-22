53cecfd76b0a49e0aa51f627447019d61e9b069b
echomicke <michael.kamara@chasacademy.se>
2023-02-22 10:41:44 +0100

    Added the first test-method

---
+++ Day1/test_part_01.py
@@ -0,0 +1,14 @@
+import unittest
+import part01 as pt1
+
+class TestStringMethods(unittest.TestCase):
+
+    def setup(self):
+        self.example_input = [12, 14, 1969, 100756]
+    
+    def test_fuel_amount(self):
+        mass = 1969
+        expected_result = 654
+        result = pt1.Fuel_required(mass)
+
+        self.assertEqual(expected_result, result)

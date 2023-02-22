5288c57eb00e2ed8a21c2164b9c8782d9ccb8774
echomicke <michael.kamara@chasacademy.se>
2023-02-22 12:39:50 +0100

    Added the second test method

--- Day2/test_part_01.py
+++ Day2/test_part_01.py
@@ -6,3 +6,8 @@ def test_pre_run():
         expected_value = [1, 12, 2, 4, 99, 5, 6, 0, 99]
         result = pt1.pre_run(example_input)
         assert expected_value == result
+
+def test_process_intcode():
+        expected_value = [30, 1, 1, 4, 2, 5, 6, 0, 99]
+        result = pt1.process_intcode(example_input)
+        assert expected_value == result

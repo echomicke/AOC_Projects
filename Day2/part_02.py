52ccf6ee3a3305271f6b9e9fed4fe8fc8ac343ff
echomicke <michael.kamara@chasacademy.se>
2023-02-22 13:36:32 +0100

    Added the Process Intcode method

---
+++ Day2/part_02.py
@@ -0,0 +1,20 @@
+def process_intcode(intcode):
+    """Processes an intcode based on the opcode"""
+    for i in range(0, len(intcode), 4):
+        if i + 3 >= len(intcode):
+            break
+
+        x = intcode[intcode[i + 1]]
+        y = intcode[intcode[i + 2]]
+        z = intcode[i + 3]
+
+        if intcode[i] == 1:
+            intcode[z] = x + y
+
+        elif intcode[i] == 2:
+            intcode[z] = x * y
+
+        elif intcode[i] == 99:
+            break
+
+    return intcode

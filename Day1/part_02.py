5cd2180f5f82e515d2f3358fd485595c20394e74
echomicke <michael.kamara@chasacademy.se>
2023-02-22 12:04:44 +0100

    Solved Day1 part2

--- Day1/part_02.py
+++ Day1/part_02.py
@@ -16,3 +16,11 @@ def Total_required_fuel(module_mass):
             temp_fuel_module = temp_fuel
     
     return fuel_sum
+
+
+if __name__ == '__main__':
+    with open("input") as file:
+        data = file.read()
+        data = data.splitlines()
+        data_list = list(map(int, data))
+        print(f"sum of the fuel requirements for all modules: {Total_required_fuel(data_list)}")

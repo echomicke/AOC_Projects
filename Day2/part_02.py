34390d61e7d9e189ece89b0c0a0fe7aabc2aed71
echomicke <michael.kamara@chasacademy.se>
2023-02-22 14:02:15 +0100

    Added the Find-Noun-Verb method

--- Day2/part_02.py
+++ Day2/part_02.py
@@ -1,4 +1,5 @@
 def process_intcode(intcode):
+    
     """Processes an intcode based on the opcode"""
     for i in range(0, len(intcode), 4):
         if i + 3 >= len(intcode):
@@ -18,3 +19,20 @@ def process_intcode(intcode):
             break
 
     return intcode
+
+def find_Noun_verb(intcode, targetOutputt):
+    result = 0
+    for noun in range(100):
+        for verb in range(100):
+            intcode[1] = noun
+            intcode[2] = verb
+            output = process_intcode(intcode)
+
+            if output[0] == targetOutputt:
+                result = 100 * noun + verb
+                break
+
+        if output[0] == targetOutputt:
+            break
+
+    return result

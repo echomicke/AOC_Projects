815d6041efd1f8150155d21695f717e15aa790e0
echomicke <michael.kamara@chasacademy.se>
2023-02-22 11:02:52 +0100

    Added the Total Fuel Required method

--- Day1/part01.py
+++ Day1/part01.py
@@ -2,3 +2,8 @@ def Fuel_required(mass):
     """Calculates and returns the amount of fuel required based on the mass"""
     fuel = mass // 3
     return fuel -2
+
+
+def Total_fuel_required(mass):
+    """Returns the sum of fuel required based on the given fuel mass"""
+    return sum(Fuel_required(x) for x in mass)
